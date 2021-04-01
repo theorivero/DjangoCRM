a7c3c0a3ff357be8b919981d71eeb735925c400e
theorivero <thrivero1@gmail.com>
2021-04-01 12:28:51 -0300

    add CRUD to Customers and Orders

---
+++ crm/accounts/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import *
+
+
+class OrderForm(ModelForm):
+	class Meta:
+		model = Order
+		fields = '__all__'
+
+class CustomerForm(ModelForm):
+	class Meta:
+		model = Customer
+		fields = '__all__'
--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import *
 
 def home(request):
 	customers = Customer.objects.all()
@@ -28,3 +29,80 @@ def customer(request, pk):
 	orders = customer.order_set.all()
 	context = {'customer': customer, 'orders': orders}
 	return render(request, 'accounts/customer.html', context)
+
+def createOrder(request):
+
+	form = OrderForm()
+	if request.method == "POST":
+		form = OrderForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context = {'form': form}
+
+	return render(request, 'accounts/order_form.html', context)
+
+
+def updateOrder(request, pk):
+
+	order = Order.objects.get(id=pk)
+	form = OrderForm(instance=order)
+	if request.method == "POST":
+		form = OrderForm(request.POST, instance=order)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+
+
+
+	context = {'form': form}
+
+	return render(request, 'accounts/order_form.html', context)
+
+
+def deleteOrder(request, pk):
+
+	order = Order.objects.get(id=pk)
+	if request.method == 'POST':
+		order.delete()
+		return redirect('/')
+
+	context = {"item": order}
+	return render(request, 'accounts/delete.html', context)
+
+
+
+def createCustomer(request):
+	form = CustomerForm()
+
+	if request.method == "POST":
+		form = CustomerForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+	context ={'form': form}
+	return render(request, 'accounts/customer_form.html', context)
+
+def updateCustomer(request, pk):
+	customer = Customer.objects.get(id=pk)
+	form = CustomerForm(instance=customer)
+	if request.method == "POST":
+		form = CustomerForm(request.POST, instance=customer)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
+
+	context ={'form': form}
+	return render(request, 'accounts/customer_form.html', context)
+
+def deleteCustomer(request, pk):
+	customer = Customer.objects.get(id=pk)
+	if request.method == 'POST':
+		customer.delete()
+		return redirect('/')
+
+
+	context = {'item': customer}
+	return render(request, 'accounts/delete.html', context)
