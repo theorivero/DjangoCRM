a34fefeb197403022406685ef82e985181bf1b92
theorivero <thrivero1@gmail.com>
2021-03-31 21:43:10 -0300

    add models and finish dashboard.html

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
+
 def home(request):
-	return render(request, 'accounts/dashboard.html')
+	customers = Customer.objects.all()
+	orders = Order.objects.all()
+
+	delivered = Order.objects.filter(status='Delivered')
+	pending = Order.objects.filter(status='Pending')
+
+	context = {
+		"customers" : customers,
+		"orders" : orders,
+		"delivered" : delivered,
+		"pending" : pending
+
+	}
+	return render(request, 'accounts/dashboard.html', context)
 	
 def products(request):
-	return render(request, 'accounts/products.html')
+	products = Product.objects.all()
+	return render(request, 'accounts/products.html', {'products': products})
 
 def customer(request):
 	return render(request, 'accounts/customer.html')
